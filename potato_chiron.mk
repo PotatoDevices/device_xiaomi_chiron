3bf2aa9c0ad62ea602035cc1fe46e2ff576b0002
HrX03 <dn.bianco03@gmail.com>
2019-06-13 11:32:12 +0200

    Potato bringup

--- potato_chiron.mk
+++ potato_chiron.mk
@@ -22,9 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/chiron/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_chiron
+PRODUCT_NAME := potato_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
